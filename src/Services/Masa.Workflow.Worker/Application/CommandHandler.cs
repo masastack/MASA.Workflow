9b331b8f967be523db49935a6989f12c774d58dc
MayueCif <mayue8373@live.com>
2023-10-08 17:12:45 +0800

    refactor:workflow service (#19)
    * update flow
    
    * feat:update flow status
    
    * feat:refactor service

--- src/Services/Masa.Workflow.Worker/Application/CommandHandler.cs
+++ src/Services/Masa.Workflow.Worker/Application/CommandHandler.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json;
+﻿using Masa.Workflow.Activities.Contracts;
 
 namespace Masa.Workflow.Worker.Application;
 
@@ -23,11 +23,41 @@ public class CommandHandler
         {
             throw new UserFriendlyException($"The workflow with id {command.WorkflowId} does not exist");
         }
+
+        await Console.Out.WriteLineAsync("--------------workflowDefinition");
         await Console.Out.WriteLineAsync(JsonSerializer.Serialize(workflowDefinition));
 
-        var workflowInstance = workflowDefinition.Adapt<WorkflowInstance>();
+        //todo change mapster
+        var workflowInstance = Convert(workflowDefinition);
+        await Console.Out.WriteLineAsync("--------------workflowInstance");
+        await Console.Out.WriteLineAsync(JsonSerializer.Serialize(workflowInstance));
 
         await _daprClient.StartWorkflowAsync(DaprWorkflowComponent, nameof(MasaWorkFlow), command.WorkflowId, workflowInstance);
+    }
+
+    private WorkflowInstance Convert(WorkflowDefinition workflowDefinition)
+    {
+        var workflowInstance = new WorkflowInstance()
+        {
+            Id = Guid.Parse(workflowDefinition.Id),
+            Name = workflowDefinition.Name,
+            StartActivity = new StartActivity(Guid.Parse(workflowDefinition.Nodes.First().Id))
+        };
+
+        foreach (var node in workflowDefinition.Nodes)
+        {
+            workflowInstance.Activities.Add(new ActivityDefinition
+            {
+                Id = Guid.Parse(node.Id),
+                Name = node.Name,
+                Disabled = node.Disabled,
+                Type = node.Type,
+                //RetryPolicy = node.RetryPolicy.Adapt<Core.Models.RetryPolicy>(),
+                Meta = JsonSerializer.Deserialize<MetaBase>(node.Meta)
+            });
+        }
+
+        return workflowInstance;
     }
 
     [EventHandler]
